c8a16d6940407bca6902e886b8c2267be2467de2
Alex <alexander.crump11@gmail.com>
2022-12-14 09:41:14 +0100

    [readingPart1]:part1 page content added

--- src/routes/reading-use-of-english/+page.svelte
+++ src/routes/reading-use-of-english/+page.svelte
@@ -1,21 +1,29 @@
+<script>
+  import ReadingPart1 from "./ReadingPart1.svelte"
+</script>
+
 <svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
+  <title>About</title>
+  <meta name="description" content="About this app" />
 </svelte:head>
 
 <div class="text-column">
-	<h1>Reading & Use of English</h1>
+  <h1>Reading & Use of English</h1>
 
-	<h2>What's the Cambridge First Certificate Reading and Use of English exam like?</h2>
+  <h2>
+    What's the Cambridge First Certificate Reading and Use of English exam like?
+  </h2>
 
-	<p>The test has seven sections and takes 75 minutes:</p>
-	<ol>
-		<li><a href="/">Part 1 - multiple choice cloze</a></li>
-		<li><a href="/">Part 2 - open cloze, text with gaps</a></li>
-		<li><a href="/">Part 3 - word formation</a></li>
-		<li><a href="/">Part 4 - key word transformations</a></li>
-		<li><a href="/">Part 5 - text with 6 multiple choice questions</a></li>
-		<li><a href="/">Part 6 - text with 6 sentences missing</a></li>
-		<li><a href="/">Part 7 - multiple matching, 10 questions</a></li>
-	</ol>
+  <p>The test has seven sections and takes 75 minutes:</p>
+  <ol>
+    <li><a href="/">Part 1 - multiple choice cloze</a></li>
+    <li><a href="/">Part 2 - open cloze, text with gaps</a></li>
+    <li><a href="/">Part 3 - word formation</a></li>
+    <li><a href="/">Part 4 - key word transformations</a></li>
+    <li><a href="/">Part 5 - text with 6 multiple choice questions</a></li>
+    <li><a href="/">Part 6 - text with 6 sentences missing</a></li>
+    <li><a href="/">Part 7 - multiple matching, 10 questions</a></li>
+  </ol>
 </div>
+
+<ReadingPart1 />
---
+++ src/routes/reading-use-of-english/ReadingPart1.svelte
@@ -0,0 +1,183 @@
+<div class="exam-part">
+  <div class="text-column">
+    <h1>Reading & Use of English - Part 1</h1>
+    <h2>
+      What is reading and use of english part 1 like in the First Certificate
+      exam?
+    </h2>
+
+    <p>
+      In reading/use of english part 1 you have a gap in the text and you have
+      to choose the correct answer from 4 options (A, B, C or D).
+    </p>
+
+    <h2>Quick tips for reading and use of english part 1</h2>
+    <ul>
+      <li>
+        Remember the first question (0) is an example, and the answer is on the
+        page. Do not get confused and answer this question.
+      </li>
+      <li>Reading lots of text in English is the best way to practise.</li>
+      <li>
+        Prepositions are very important (prepositions examples are: <span
+          class="highlight">of, to, with, out, in</span
+        >
+        etc.)
+      </li>
+      <li>
+        Word pairings/phrases are also important, so look at the words before
+        and after to help.
+      </li>
+      <li>
+        Finally, slight differences in similar words are also important, for
+        example, the following words have slightly different meanings;
+        <span class="highlight">moving, trembling, jumping, rattling</span> and are
+        more appropriate for certain specific things than others.
+      </li>
+    </ul>
+  </div>
+</div>
+
+<div class="example">
+  <div class="text-column">
+    <h1>Example Questions</h1>
+    <p>
+      For questions 1-8, read the text below and decide which answer (A, B, C or
+      D) best fits each gap.
+    </p>
+    <hr />
+    <div class="reading-q">
+      <h3>What is genealogy?</h3>
+      <p>
+        Genealogy is branch of history. It concerns family history <strong
+          >(1) ........</strong
+        >
+        than the national or world history studied at school. It doesn't merely involved
+        drawing a family tree, hiowever - tracing your family history can also
+        <strong>(2) ........</strong> in learning about your roots and your identity.
+      </p>
+
+      <table>
+        <tr>
+          <td><span>1</span></td>
+          <td><span>a</span> instead</td>
+          <td><span>b</span> rather</td>
+          <td><span>c</span> except</td>
+          <td><span>d</span> sooner</td>
+        </tr>
+        <tr class="list-row">
+          <td><span>2</span></td>
+          <td><span>a</span> cause</td>
+          <td><span>b</span> mean</td>
+          <td><span>c</span> result</td>
+          <td><span>d</span> lead</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+  <div class="transcript">
+    <div class="text-column">
+      <h1>Answers</h1>
+      <h2>Number (1)</h2>
+      <p>
+        We have a comma before the space, so it is a new clause. We need to
+        focus on the word after the gap; <span class="highlight2">than</span>.
+      </p>
+      <p>So our options are:</p>
+      <table>
+        <tr>
+          <td />
+          <td><span>a</span> instead than</td>
+          <td><span>b</span> rather than</td>
+          <td><span>c</span> except than</td>
+          <td><span>d</span> sooner than</td>
+        </tr>
+      </table>
+      <p>
+        Two of these pairings do not exist in English (A and C), so now we have
+        to look at the meaning between (B) and (D).
+      </p>
+      <p>
+        (B) <span class="highlight2">Rather than</span> means to chose/refer to something
+        other than what immediately follows. E.G. I prefer swimming in the sea rather
+        than the pool. Whatever follows rather than is to be considered not important
+        compared to the other option in the sentence.
+      </p>
+      <p>
+        (D) <span class="highlight2">(No) sooner… than</span> means that something
+        happens immediately after another action, but ‘sooner’ and ‘than’ are separated.
+        e.g. I had no sooner arrived than everybody left the party.
+      </p>
+      <p>
+        Clearly the answer is (B) for this question. (No) sooner… than is quite
+        high level, so don’t worry too much about understanding this, but
+        realise it is not the correct answer here.
+      </p>
+      <h2>Number (2)</h2>
+      <p>
+        Before the space is the linker <span class="highlight2">also</span>.
+        This means we are looking for a clause that has a similar meaning to the
+        first clause, for this question it is not that important but it in
+        others it might be. After the space is the preposition
+        <span class="highlight2">in</span>.
+      </p>
+
+      <p>Our options are:</p>
+      <table>
+        <tr>
+          <td />
+          <td><span>a</span> cause in</td>
+          <td><span>b</span> mean in</td>
+          <td><span>c</span> result in</td>
+          <td><span>d</span> lead in</td>
+        </tr>
+      </table>
+      <p>
+        Once again, two of these pairings do not exist in English (A and B), so
+        we need to look at the meanings of (C) and (D).
+      </p>
+      <p>
+        (C) <span class="highlight2">Result in</span> is a phrasal verb which means
+        to cause a particular situation to happen, eg. The flood resulted in damage
+        to their house.
+      </p>
+      <p>
+        (D) <span class="highlight2">Lead in</span> is a noun which means something
+        that introduces something else, eg. A lead in activity to an English class
+        could be a quick game.
+      </p>
+      <p>Once again, the answer should now be clear; (C).</p>
+    </div>
+  </div>
+</div>
+
+<style>
+  table,
+  tr,
+  td {
+    font-family: var(--font-text);
+    font-weight: 100;
+  }
+
+  tr td:first-child {
+    width: 2%;
+  }
+
+  td {
+    width: 10%;
+  }
+
+  .highlight,
+  .highlight2 {
+    background-color: #8ac6fd;
+    color: var(--color-theme-1);
+    font-weight: 900;
+    font-style: italic;
+    padding: 0 0.5rem;
+  }
+
+  .highlight2 {
+    background-color: white;
+    color: var(--color-theme-1);
+  }
+</style>
